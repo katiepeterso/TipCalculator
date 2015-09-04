3ec24787ca0ab061953af27b69b6f33c4484fe03
katiepeterso <katiepeterso@outlook.com>
2015-09-04 10:55:43 -0700

    first commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Katherine Peterson on 2015-09-04.
+//  Copyright (c) 2015 KatieExpatriated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TipCalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TipCalculator
+//
+//  Created by Katherine Peterson on 2015-09-04.
+//  Copyright (c) 2015 KatieExpatriated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
